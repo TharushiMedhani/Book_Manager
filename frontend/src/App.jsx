a7446fcdca9737dccb9f2837e994c179c56afda9
TharushiMedhani <tharushimedhani123@gmail.com>
2026-04-27 12:30:39 +0530

    update url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_BASE_URL = 'http://bookmanager-production-1393.up.railway.app/api';
+
 
 function App() {
   const [books, setBooks] = useState([]);
